4249ca9211795109f57539f07419594fef1f8f73
suzakutakumi <suzakutakumi3@gmail.com>
2021-10-27 20:43:03 +0900

    順列の実装

--- main.c
+++ main.c
@@ -20,5 +20,13 @@ int main() {
    if (c=='/'){
       printf("%d\n",a/b);
    }
+
+   if(c=='P'){
+      int ans=1;
+      for(int i=1;i<=b;i++){
+         ans*=a--;
+      }
+      printf("%d\n",ans);
+   }
    return 0;
 }
